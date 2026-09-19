c47d7a4411592b2fd2357239289c8be365beffec
Its-Nubz <99380579+Its-Nubz@users.noreply.github.com>
2026-09-18 18:37:27 -0700

    Move remaining photo inventory into organized data folder

---
+++ data/inventory/photo-products.js
@@ -0,0 +1,4 @@
+window.BDL_PRODUCTS = window.BDL_PRODUCTS || [];
+window.BDL_PRODUCTS.unshift(
+  {brand:"PZ",product:"Arctic Essence Age-Defying Miracle Powder Serum",size:"1.7 fl oz",retail:"$900",price:"",categoryLabel:"Skincare / Serum",categories:["new","skincare"],status:"1 Available",image:""}
+);
