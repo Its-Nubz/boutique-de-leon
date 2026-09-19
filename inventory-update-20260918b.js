7fa06a307d0c1536e2224a64d59a5e42d59acfd1
Its-Nubz <99380579+Its-Nubz@users.noreply.github.com>
2026-09-18 17:30:08 -0700

    Update Predire Vitamin C & E toner image and price

--- inventory-update-20260918b.js
+++ inventory-update-20260918b.js
@@ -11,7 +11,7 @@
   setStatus("ADORE COSMETICS", "Essence Facial Milk", "1 Available");
 
   const additions = [
-    {brand:"PRÉDIRÉ PARIS",product:"Vitamin C & E Booster Facial Toner",size:"250 mL / 8.45 fl oz",retail:"$165",price:"",categoryLabel:"Skincare / Toner",categories:["new","skincare"],status:"1 Available",image:""},
+    {brand:"PRÉDIRÉ PARIS",product:"Vitamin C & E Booster Facial Toner",size:"250 mL / 8.45 fl oz",retail:"$165",price:"$65",categoryLabel:"Skincare / Toner",categories:["new","skincare"],status:"1 Available",image:"images/products/Predire Paris Vitamin C & E Booster Facial Toner.png"},
     {brand:"BELOW ZERO",product:"Frozen Gold Multi-Vitamin Magnetic Mud Mask",size:"50 mL / 1.7 fl oz",retail:"$950",price:"",categoryLabel:"Skincare / Face Mask",categories:["new","skincare"],status:"1 Available",image:""},
     {brand:"BELOW ZERO",product:"Frozen Gold Firming & Tightening Peeling Mask with Multi-Peptide & Caffeine",size:"100 g / 3.53 fl oz",retail:"$3,750",price:"",categoryLabel:"Skincare / Peel-Off Mask",categories:["new","skincare"],status:"1 Available",image:""},
     {brand:"PRÉDIRÉ PARIS",product:"Daily Defence Vitamin C Brightening Dropper",size:"30 mL / 1 fl oz",retail:"$299.99",price:"",categoryLabel:"Skincare / Serum",categories:["new","skincare"],status:"1 Available",image:""},
